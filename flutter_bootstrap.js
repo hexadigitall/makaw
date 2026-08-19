a1c7882e24420154df4db040546b6638703504c8
hexadigitall <hexadigitall@users.noreply.github.com>
2026-08-19 20:23:48 +0000

    deploy: 9de77f060810006fe1d9e2b93175afb31ac5fb01

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4131978545" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2714322095" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
